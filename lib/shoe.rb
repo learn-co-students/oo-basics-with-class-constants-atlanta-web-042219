a93ddc424d3d7e9d06afa609997d6cc8c3c91626
github username <github email address>
2019-04-24 15:09:01 -0400

    Shoes only accepts new shoes

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,10 +1,16 @@
+require 'pry'
+
 class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
 
   def initialize(brand)
     @brand = brand
+    # if BRANDS == BRANDS.uniq!
+    BRANDS << brand unless BRANDS.include?(brand)
   end
+
+  BRANDS = []
 
   def cobble
     self.condition = "new"
